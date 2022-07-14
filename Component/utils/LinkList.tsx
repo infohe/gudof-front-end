506fd5091d7fb5064bddb83358c931e9bde47c90
iyas <iyasmvr@gmail.com>
2022-07-14 19:59:50 +0530

    checked spelling

--- Component/utils/LinkList.tsx
+++ Component/utils/LinkList.tsx
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
 import { useState } from "react";
 import FirstFolder from "./FirstFolder";
+import { resultDataFrom } from "../../back_end/filteredItems";
 
+import FilterPop from "./popup/FilterPop";
 import { Backdrop } from "../utils/popup/Backdrop";
 
 const LinkList = (props) => {
@@ -25,7 +27,13 @@ const LinkList = (props) => {
         <div>{props.item2}</div>
       </div>
       {isFilterOpen && (
-        <FirstFolder Details={""} cancelPopUp={cancelPopUp}></FirstFolder>
+        <FirstFolder Details={""} cancelPopUp={cancelPopUp}>
+          <FilterPop
+            values={resultDataFrom}
+            currentValue={props.item1}
+            head={"Add to Filter"}
+          ></FilterPop>
+        </FirstFolder>
       )}
       {isFilterOpen && <Backdrop cancelPopUp={cancelPopUp}></Backdrop>}
     </Fragment>
@@ -33,11 +41,3 @@ const LinkList = (props) => {
 };
 
 export default LinkList;
-
-{
-  /* <FilterPop
-values={resultDataFrom}
-currentValue={props.item1}
-head={"Add to Filter"}
-></FilterPop> */
-}
