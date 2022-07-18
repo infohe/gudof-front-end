37d1655bd87eac463919010e5d32b15b2a098f43
iyas <iyasmvr@gmail.com>
2022-07-18 21:12:18 +0530

    added productDetails will show part

--- Component/utils/DetailTable.tsx
+++ Component/utils/DetailTable.tsx
@@ -11,31 +11,33 @@ const DetailTable = (props) => {
   const checkingPoint = (key, field) => {
     const string_facets = [];
     if (typeof key === "string" && typeof field === "string") {
+      console.log("ENTERED STRING");
       string_facets.push({ facet_name: key, facet_value: field });
     } else {
       if (field !== null) {
         const innerObj: Array<any> = Object.entries(field);
-
         innerObj.map((entry, i) => {
           if (typeof entry === "string") {
           } else if (typeof entry === "object") {
             if (Array.isArray(entry)) {
               const { label, value } = entry[1];
-              if (value === "string") {
+              if (typeof value === "string") {
                 string_facets.push({ facet_name: label, facet_value: value });
-                // console.log(string_facets);
               }
             }
           }
         });
       }
     }
+    return string_facets;
   };
-
+  const checkPointResult = entries.map((entry) => {
+    return checkingPoint(entry[0], entry[1]);
+  });
   ///
+  console.log(checkPointResult);
 
   const CheckProperty = (Property) => {
-
     if (typeof Property === "string") {
       if (Property === "undefined undefined") {
         return (Property = "Null");
@@ -51,9 +53,49 @@ const DetailTable = (props) => {
     let finalResult = result.charAt(0).toUpperCase() + result.slice(1);
     return finalResult;
   };
-  return (
-    <Fragment>
-      {entries.map((entry, i) => (
+  return checkPointResult.map((innerArrays) => {
+    return innerArrays.map((itemArrays, i) => {
+      // con(sole.log(itemArrays.facet_name, itemArrays.facet_value);
+      return (
+        <div className="grid grid-cols-2	auto-cols-fr  text-sm		" key={i}>
+          <div className="shadow-sm rounded-sm border border-gray-200 p-2">
+            {SentenceCase(itemArrays.facet_name)}
+            {/* {SentenceCase(entry[0])} */}
+            {/* {console.log(itemArrays)} */}
+          </div>
+          <div
+            className="shadow-sm rounded-sm border border-gray-200 p-2"
+            key={i}
+          >
+            {itemArrays.facet_value}
+
+            {/* {CheckProperty(entry[1])} */}
+          </div>
+        </div>
+      );
+    });
+  });
+};
+
+export default DetailTable;
+
+// {results.map((itemArray, i) => {
+//   item.map((innerArrays) => {
+//     <div className="grid grid-cols-2	auto-cols-fr  text-sm		" key={i}>
+//       <div className="shadow-sm rounded-sm border border-gray-200 p-2">
+//         {SentenceCase(entry[0])}
+//       </div>
+//       <div
+//         className="shadow-sm rounded-sm border border-gray-200 p-2"
+//         key={i}
+//       >
+//         {CheckProperty(entry[1])}
+//       </div>
+//     </div>;
+//   });
+// })}
+{
+  /* {entries.map((entry, i) => (
         <div className="grid grid-cols-2	auto-cols-fr  text-sm		" key={i}>
           <div className="shadow-sm rounded-sm border border-gray-200 p-2">
             {SentenceCase(entry[0])}
@@ -64,10 +106,5 @@ const DetailTable = (props) => {
           >
             {CheckProperty(entry[1])}
           </div>
-        </div>
-      ))}
-    </Fragment>
-  );
-};
-
-export default DetailTable;
+        </div> */
+}
